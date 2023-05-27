cbb78661c1141dbe6a8c64bd26653b1c30fac1f0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-05-27 14:45:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import os
 from uclchem.src import uclchem
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return "<h1>UCLCHEM API - Working</h1>"
+
 @app.route("/api/simple_model", methods=['POST'])
 def api():
     data: dict = request.get_json()
     out_species = data.pop('out_species') if 'out_species' in data else None
     status, *abundances = uclchem.model.cloud(param_dict=data, out_species=out_species)
-    return jsonify({'result': status, 'abundances': abundances})
+    
+    if status < 0:
+        return jsonify({'status': status, 'results': uclchem.utils.check_error(status)})
+        
+    return jsonify({'status': status, 'results': {'abundaces': abundances}})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-# [END run_helloworld_service]
-# [END cloudrun_helloworld_service]
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
