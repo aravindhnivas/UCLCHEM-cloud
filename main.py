530494c768488f0f2dd753e6379c392a67e3cf26
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-05-27 20:00:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from uclchem.src import uclchem
 from pathlib import Path as pt
 import os
-
+from time import perf_counter
 
 app = Flask(__name__)
 output_dir = pt('./outputs')
@@ -13,25 +13,40 @@ def main():
 
 @app.route("/api/simple_model", methods=['POST'])
 def api():
+    
+    if not output_dir.exists():
+        output_dir.mkdir()
+        
     parameters: dict = request.get_json()
     out_species = parameters.pop('out_species') if 'out_species' in parameters else None
     
-    for key in ['outputFile', 'abundSaveFile', 'columnFile']:
+    for key in ['outputFile', 'abundSaveFile']:
         if key in parameters:
-            if not output_dir.exists():
-                output_dir.mkdir()
             parameters[key] = str(output_dir / parameters[key])
-            
+        else:
+            parameters[key] = str(output_dir / f'{key}.dat')
+    
+    if 'out_species' in parameters:
+        if not 'columnFile' in parameters:
+            parameters['columnFile'] = str(output_dir / 'columnFile.dat')
+    
+    time_start = perf_counter()
     status, *abundances = uclchem.model.cloud(param_dict=parameters, out_species=out_species)
+    final_time = perf_counter() - time_start
     
     if status < 0:
         return jsonify({'status': status, 'results': uclchem.utils.check_error(status)})
     
-    results = {'status': status, 'results': {'abundaces': abundances}}
+    results = {'status': status, 'results': {'abundaces': abundances}, 'time': final_time}
     
-    if 'outputFile' in parameters:
-        df = uclchem.analysis.read_output_file(parameters['outputFile'])
-        results['results']['full_output'] = df.to_json()
+    df = uclchem.analysis.read_output_file(parameters['outputFile'])
+    results['results']['full_output'] = df.to_json()
+    
+    if not 'element_list' in parameters:
+        parameters['element_list'] = ['H', 'N', 'C', 'O', 'S']
+    
+    conservation=uclchem.analysis.check_element_conservation(df, element_list=parameters['element_list'])
+    results['results']['conservation'] = conservation
         
     return jsonify(results)
 
